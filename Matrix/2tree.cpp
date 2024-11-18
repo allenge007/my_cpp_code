d1a5cfc1dc38a3d6256f6bc5240f8aad0b70c72b
__allenge <2438745244@qq.com>
2024-11-18 19:53:39 +0800

    Refactor 2tree.cpp to improve readability and performance

--- Matrix/2tree.cpp
+++ Matrix/2tree.cpp
@@ -1,64 +1,60 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-vector<TreeNode*> nodes;
-
-TreeNode* convertToBinaryTree(const vector<vector<int>>& children, int root) {
-    if (children[root].empty()) {
-        return nodes[root];
-    }
-    nodes[root]->left = nodes[children[root][0]];
-    TreeNode* current = nodes[root]->left;
-    for (size_t i = 1; i < children[root].size(); ++i) {
-        current->right = nodes[children[root][i]];
-        current = current->right;
-    }
-    for (int child : children[root]) {
-        convertToBinaryTree(children, child);
-    }
-    return nodes[root];
+const int maxn = 1e3 + 5;
+const double eps = 1e-5;
+struct node{
+    double l1, l2, r1, r2;
+    int s1, s2, s3, s4, point;
+}t[maxn << 4];
+int n, root, cnt;
+pair<double, double> a[maxn];
+vector<int> dep[maxn];
+bool in(double x, double y, double l1, double r1, double l2, double r2) {
+    return x >= l1 && x <= l2 && y >= r1 && y <= r2;
 }
-
-void levelOrderTraversal(TreeNode* root) {
-    if (!root) return;
-    queue<TreeNode*> q;
-    q.push(root);
-    while (!q.empty()) {
-        TreeNode* node = q.front();
-        q.pop();
-        cout << node->val << " ";
-        if (node->left) q.push(node->left);
-        if (node->right) q.push(node->right);
+int check(double l1, double r1, double l2, double r2) {
+    int num = 0;
+    for(int i = 1; i <= n; i ++) {
+        if(in(a[i].first, a[i].second, l1, r1, l2, r2)) num ++;
     }
-    cout << endl;
+    return num;
 }
-
+void build(int &p, double l1, double r1, double l2, double r2) {
+    if(!p) p = ++ cnt;
+    t[p].l1 = l1, t[p].r1 = r1, t[p].l2 = l2, t[p].r2 = r2;
+    if(check(l1, r1, l2, r2) == 1) {
+        for(int i = 1; i <=n ; i ++) {
+            if(in(a[i].first, a[i].second, l1, r1, l2, r2)) t[p].point = i;
+        }
+        return ;
+    }
+    double mid1 = (l1 + l2) / 2, mid2 = (r1 + r2) / 2;
+    if(check(l1 + eps, mid2 + eps, mid1, r2)) build(t[p].s1, l1 + eps, mid2 + eps, mid1, r2);
+    if(check(mid1 + eps, mid2 + eps, l2, r2)) build(t[p].s2, mid1 + eps, mid2 + eps, l2, r2);
+    if(check(l1 + eps, r1 + eps, mid1, mid2)) build(t[p].s3, l1 + eps, r1 + eps, mid1, mid2);
+    if(check(mid1 + eps, r1 + eps, l2, mid2)) build(t[p].s4, mid1 + eps, r1 + eps, l2, mid2);
+}
+int maxd;
+void output(int p, int d) {
+    if(!p) return ;
+    if(t[p].point) dep[d].push_back(t[p].point), maxd = max(maxd, d);
+    output(t[p].s1, d + 1);
+    output(t[p].s2, d + 1);
+    output(t[p].s3, d + 1);
+    output(t[p].s4, d + 1);
+}
 int main() {
-    int n;
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
     cin >> n;
-    nodes.resize(n + 1);
-    for (int i = 1; i <= n; ++i) {
-        nodes[i] = new TreeNode(i);
+    for(int i = 1; i <= n; i++) {
+        cin >> a[i].first >> a[i].second;
     }
-    vector<vector<int>> children(n + 1);
-    for (int i = 2; i <= n; ++i) {
-        int parent;
-        cin >> parent;
-        children[parent].push_back(i);
-    }
-
-    TreeNode* root = convertToBinaryTree(children, 1);
-    levelOrderTraversal(root);
-
+    root = cnt = 1;
+    build(root, -eps, -eps, 10 + eps, 10 + eps);
+    output(root, 0);
+    for(int i = 0; i <= maxd; i ++)
+        for(auto p : dep[i]) cout << a[p].first << " " << a[p].second << "\n";
     return 0;
 }
